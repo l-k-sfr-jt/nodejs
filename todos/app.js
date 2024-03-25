ad97d632bfe8cfa5e86ff7b337efd3c48dcb50fa
l-k-sfr-jt <lukas3197@gmail.com>
2024-03-25 23:18:17 +0100

    cv 07

--- todos/app.js
+++ todos/app.js
@@ -1,71 +1,85 @@
 import express from "express";
+import knex from 'knex'
+import knexfile from './knexfile.js'
 
 const port = 3000;
 
 const app = express();
-
-const todos = [
-  { id: 1, title: "First Todo", done: false },
-  { id: 2, title: "Second Todo", done: true }
-];
+const db = knex(knexfile);
 
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: false }))
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.use((req, res, next) => {
+    console.log('Incomming request', req.method, req.url)
+    next()
 });
 
-app.get("/", (req, res) => {
+
+app.get("/", async (req, res) => {
+    const todos = await db().select('*').from('todos')
     res.render("index", {
+        title: 'ToDos!',
         todos
     });
-    console.log(todos);
 });
 
-app.get("/todo/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const todo = todos.find(todo => todo.id === id);
-    if(!todo) return res.redirect("/nenalezeno");
+app.get("/todo/:id", async (req, res, next) => {
+    const todo = await db('todos').select('*').where('id', req.params.id).first();
+
+    if (!todo) return next()
+
     res.render("todo/todo-detail", {
         todo
     });
 });
 
-app.post("/add-todo", (req, res) => {
-    const todo = req.body;
-    todos.push({
-        id: todos.length + 1,
-        title: todo.title,
-        done: false
-    });
+app.post("/add-todo", async (req, res) => {
+    const text = String(req.body.text);
+    await db('todos').insert({ text });
     res.redirect("/");
 });
 
-app.post("/toggle-todo/:id", (req, res) => {
-    const id = req.params.id;
-    const todo = todos.find(todo => todo.id === Number(id));
-    todo.done = !todo.done;
+app.post("/toggle-todo/:id", async (req, res, next) => {
+    const todo = await db('todos').select('*').where('id', req.params.id).first();
+
+    if (!todo) return next();
+
+    await db('todos').update({ done: !todo.done }).where('id', todo.id);
     res.redirect('back');
 });
 
-app.post("/edit-todo/:id", (req, res) => {
-    const todoData = req.body;
-    const todo = todos.find(todo => todo.id === Number(req.params.id));
-    todo.title = todoData.title;
-    todo.done = todoData.done === "on";
+app.post("/edit-todo/:id", async (req, res, next) => {
+    const todo = await db('todos').select('*').where('id', req.params.id).first();
+
+    if (!todo) return next();
+
+    await db('todos').update({ text: req.body.text }).where('id', todo.id)
     res.redirect("/todo/" + todo.id);
 });
 
-app.post("/remove-todo/:id", (req, res) => {
-    const id = req.params.id;
-    const todoIndex = todos.findIndex(todo => todo.id === Number(id));
-    todos.splice(todoIndex, 1);
+app.post("/remove-todo/:id", async (req, res, next) => {
+    const todo = await db('todos').select('*').where('id', req.params.id).first();
+
+    if (!todo) return next();
+
+    await db('todos').delete().where('id', todo.id)
+
     res.redirect("/");
 });
 
-app.get("/nenalezeno", (req, res) => {
-    res.status(404).render("404");
+app.use((req, res) => {
+    res.status(404);
+    res.send('404 - Stránka nenalezena');
 });
 
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500);
+    res.send('500 - Chyba na straně serveru');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
